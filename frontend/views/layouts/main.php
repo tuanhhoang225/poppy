e34e00ed525e19354b7b90908a8cb356e6166ec0
Ngo Thuy Linh <linhngothithuy96@gmail.com>
2018-04-21 18:07:37 +0700

    giao dien

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -11,26 +11,130 @@ use frontend\assets\ThemeAsset;
 ThemeAsset::register($this);
 
 ?>
+
+
 <?php $this->beginPage() ?>
+
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="vi"
+      class=" html_stretched responsive av-preloader-disabled av-default-lightbox  html_header_top html_logo_center html_bottom_nav_header html_menu_right html_slim html_header_sticky html_header_shrinking html_header_transparency html_mobile_menu_phone html_disabled html_header_searchicon_disabled html_content_align_center html_header_unstick_top html_header_stretch html_entry_id_10 ">
+
+<!-- Mirrored from romanplaza.vn/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Apr 2018 06:14:10 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8"/><!-- /Added by HTTrack -->
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta name="description" content="LearnPress | Education & Courses HTML Template"/>
+    <meta name="keywords" content="academy, course, education, education html theme, elearning, learning,"/>
+    <meta name="author" content="ThemeMascot"/>
+
+    <!-- Page Title -->
+    <title>LearnPress | Education & Courses HTML Template</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+    <link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+    <!-- Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/css-plugin-collections.css" rel="stylesheet"/>
+    <!-- CSS | menuzord megamenu skins -->
+    <link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-rounded-boxed.css" rel="stylesheet"/>
+    <!-- CSS | Main style file -->
+    <link href="css/style-main.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Preloader Styles -->
+    <link href="css/preloader.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Custom Margin Padding Collection -->
+    <link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Responsive media queries -->
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+    <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+    <!-- Revolution Slider 5.x CSS settings -->
+    <link href="js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+    <link href="js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
+    <link href="js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
+
+    <!-- CSS | Theme Color -->
+    <link href="css/colors/theme-skin-color-set-1.css" rel="stylesheet" type="text/css">
+
+    <!-- external javascripts -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- JS | jquery plugin collection for this theme -->
+    <script src="js/jquery-plugin-collection.js"></script>
+
+    <!-- Revolution Slider 5.x SCRIPTS -->
+    <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body>
-<?php $this->beginBody() ?>
 
-<?= $this->render('header-main') ?>
+<!--<body id="top"-->
+<!--      class="home page-template-default page page-id-10 page-parent stretched helvetica-neue-websave _helvetica_neue "-->
+<!--      itemscope="itemscope" itemtype="https://schema.org/WebPage">-->
+<!---->
+<!---->
+<!---->
+<!--<div id='wrap_all'>-->
+<!---->
 
-<?= $content ?>
+
+    <?= $content ?>
 
 
-<?php $this->endBody() ?>
+
+<script type='text/javascript' src='/wp-content/plugins/contact-form-7/includes/js/scripts20fd.js?ver=4.9.2'></script>
+<script type='text/javascript'
+        src='/wp-content/plugins/wp-expand-tabs-free/js/jquery.pwstabs-1.1.3.min68b3.js?ver=1'></script>
+<script type='text/javascript' src='/wp-content/themes/landmark/js/avia592e.js?ver=3'></script>
+<script type='text/javascript' src='/wp-content/themes/landmark/js/shortcodes592e.js?ver=3'></script>
+<script type='text/javascript'
+        src='/wp-content/themes/landmark/js/aviapopup/jquery.magnific-popup.min77ae.js?ver=2'></script>
+<script type='text/javascript' src='/wp-includes/js/mediaelement/wp-mediaelement.min6959.js?ver=4.9.3'></script>
+<script type='text/javascript' src='/wp-includes/js/comment-reply.min6959.js?ver=4.9.3'></script>
+<script type='text/javascript' src='/wp-includes/js/wp-embed.min6959.js?ver=4.9.3'></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109863655-4"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'UA-109863655-4');
+</script>
+<a href='#top' title='Scroll to top' id='scroll-top-link' aria-hidden='true' data-av_icon=''
+   data-av_iconfont='entypo-fontello'><span class="avia_hidden_link_text">Scroll to top</span></a>
+
+<div id="fb-root"></div>
+
+
+
+
 </body>
+
+<!-- Mirrored from romanplaza.vn/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Apr 2018 06:21:45 GMT -->
 </html>
+
+
 <?php $this->endPage() ?>
